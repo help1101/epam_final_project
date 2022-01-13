316de1b56a1770388e7e1a1a7f1d8126630b4850
help1101 <kucenko1998@gmail.com>
2022-01-13 14:45:00 +0200

    second commit, app v1.0

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup
 setup(
     name='Departments app project',
     version='1.0',
-    packages=['departments_app', 'departments_app.views', 'departments_app.models', 'departments_app.service'],
-    url='',
-    license='',
+    description='Web application to manage departments and employees.',
     author='Mykola Kutsenko',
     author_email='kucenko1998@gmail.com',
-    description=''
+    url='https://github.com/help1101/epam_final_project',
+    packages=['departments_app', 'departments_app.views', 'departments_app.models', 'departments_app.service']
 )
